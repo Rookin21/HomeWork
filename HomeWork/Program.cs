9ca3ae291d2af3e512c3bb6d0156014378754621
Almas <Crazynick21@gmail.com>
2022-03-05 08:32:29 +0300

    Домашнее задание. Урок 2. Задание 1.

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -6,7 +6,19 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите минимальную температуру за сутки");
 
+            float minTemp = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Введите максимальную температуру за сутки");
+
+            float maxTemp = Convert.ToInt64(Console.ReadLine());
+
+            float averageTemp = (minTemp + maxTemp) / 2;
+
+            Console.WriteLine($"Среднесуточная температура за сутки равна: {averageTemp}");
+
+            Console.Read();
         }
     }
 }
